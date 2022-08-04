e09a49c48b799f681196674c4489e6d26c9fb0d8
DCodeMania <wolfmania44@gmail.com>
2022-08-04 15:33:07 +0530

    new file:   .gitignore new file:   index.html new file:   package-lock.json new file:   package.json new file:   public/css/bootstrap.css new file:   public/css/main.css new file:   public/js/main.js new file:   public/scss/bootstrap.scss new file:   public/scss/main.scss

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+const showSidebarBtn = document.querySelector('#show_sidebar_phone');
+const showSidebarBtnPc = document.querySelector('#show_sidebar_pc');
+const closeSidebarBtn = document.querySelector('#close_sidebar');
+
+showSidebarBtn.onclick = function () {
+  const sidebar = document.querySelector('.wrapper');
+  sidebar.classList.toggle('show');
+}
+
+closeSidebarBtn.onclick = function () {
+  const sidebar = document.querySelector('.wrapper');
+  sidebar.classList.remove('show');
+}
+
+showSidebarBtnPc.onclick = function () {
+  const sidebar = document.querySelector('.wrapper');
+  sidebar.classList.toggle('show_pc');
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
+
+
