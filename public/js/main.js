debe02c84f8c6612ccb583327db98dd191bac3f7
DCodeMania <wolfmania44@gmail.com>
2022-08-05 14:49:19 +0530

    modified:   index.html modified:   public/css/main.css modified:   public/js/main.js modified:   public/scss/main.scss

--- public/js/main.js
+++ public/js/main.js
@@ -2,27 +2,40 @@ const showSidebarBtn = document.querySelector('#show_sidebar_phone');
 const showSidebarBtnPc = document.querySelector('#show_sidebar_pc');
 const closeSidebarBtn = document.querySelector('#close_sidebar');
 const overlay = document.querySelector('#overlay');
+const fullscreen = document.querySelector('#fullscreen');
+const wrapper = document.querySelector('.wrapper');
 
 showSidebarBtn.onclick = function () {
-  const sidebar = document.querySelector('.wrapper');
-  sidebar.classList.toggle('show');
+  wrapper.classList.toggle('show');
 }
 
 closeSidebarBtn.onclick = function () {
-  const sidebar = document.querySelector('.wrapper');
-  sidebar.classList.remove('show');
+  wrapper.classList.remove('show');
 }
 
 overlay.onclick = function () {
-  const sidebar = document.querySelector('.wrapper');
-  sidebar.classList.remove('show');
+  wrapper.classList.remove('show');
 }
 
 showSidebarBtnPc.onclick = function () {
-  const sidebar = document.querySelector('.wrapper');
-  sidebar.classList.toggle('show_pc');
-  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  wrapper.classList.toggle('show_pc');
 }
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+fullscreen.onclick = function () {
+  const i = document.querySelector('#fullscreen i');
+  if (i.classList.contains('fa-expand')) {
+    i.classList.add('fa-compress');
+    i.classList.remove('fa-expand');
+    wrapper.requestFullscreen();
+  } else {
+    i.classList.add('fa-expand');
+    i.classList.remove('fa-compress');
+    document.exitFullscreen();
+  }
+}
+
 
 
